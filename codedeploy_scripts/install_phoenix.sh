fb972728bcba26d8e164b2ecc87cc95f66d40a3e
thash <hash@hzl.jp>
2017-03-20 14:19:52 +0900

    export HOME and check commands

--- codedeploy_scripts/install_phoenix.sh
+++ codedeploy_scripts/install_phoenix.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
+### check
+echo 'check id/env >>>'
+id
+pwd
+env
+echo 'check id/env <<<'
 
-# Install Erlang http://elixir-lang.org/install.html#installing-erlang
+### Install Erlang http://elixir-lang.org/install.html#installing-erlang
 yum install -y erlang
 erl +V
 # => expected output example: Erlang (SMP,ASYNC_THREADS,HIPE) (BEAM) emulator version 8.2.2
 
 
-# http://elixir-lang.org/install.html#compiling-from-source-unix-and-mingw
+### http://elixir-lang.org/install.html#compiling-from-source-unix-and-mingw
 curl -L -O https://github.com/elixir-lang/elixir/archive/v1.4.2.tar.gz
 tar xzvf v1.4.2.tar.gz
 cd elixir-1.4.2
+# [stderr]erlexec: HOME must be set
+export HOME=/root
 make clean test
 make install
 
