90eaf445b28784bf74d3ae404e3365046132418e
thash <hash@hzl.jp>
2017-03-20 21:13:47 +0900

    cd

--- codedeploy_scripts/phoenix_server.sh
+++ codedeploy_scripts/phoenix_server.sh
@@ -9,4 +9,6 @@ echo "env ------------------\n"
 env
 echo "phoenix.server ------------------\n"
 export HOME=/root
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/
+pwd
 PORT=80 MIX_ENV=prod mix phoenix.server
